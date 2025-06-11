1327c022553bc0ddea4c2e77da7791a2e504ffcb
Jose Angel Audelo Araoz <jose.audelo@vasscompany.com>
2025-06-11 16:41:50 -0600

    first commit

---
+++ src/test/java/com/santander/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santander.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
